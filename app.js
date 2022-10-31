c08857bdf7bbd8106e7c1d3505de0f6d0cd824b0
Olya Y <f_akora@mail.ru>
2022-11-01 00:28:55 +0200

    refactor: do better semantics tags (rule 1.1 of html-and-css-extended.md)

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 //New task list item
 var createNewTaskElement=function(taskString){
 
-    var listItem=document.createElement("li");
+    var listItem=document.createElement("article");
     listItem.classList.add("list");
 
     //input (checkbox)
